5e44ef9fb90ed875f05f0d5ca3085729d603a5fe
rst515 <mmrust515@gmail.com>
2025-09-09 09:24:24 +0100

    Replace prospector with ruff in pre-push script and add ruff configuration file.

--- tools/dev/pre_push.sh
+++ tools/dev/pre_push.sh
@@ -2,26 +2,24 @@
 
 # Run from command line with ./tools/dev/pre_push.sh  (requires chmod +x)
 
-lt_yellow   () { printf "\e[33m"      ; "$@" ; printf "\e[0m"; }
-dk_green    () { printf "\e[38;5;35m" ; "$@" ; printf "\e[0m"; }
 purple      () { printf "\e[35m"      ; "$@" ; printf "\e[0m"; }
 
-purple echo 'Running pre-push checks...' && \
+purple echo 'Running pre-push checks...' && echo && \
 
-lt_yellow echo 'Running prospector checks for information about errors, potential problems, convention violations and complexity...' && \
-prospector  --max-line-length 120 && \
+purple echo 'Running ruff code quality checks for information about errors, potential problems, convention violations and complexity...' && \
+ruff check . && echo && \
 
-lt_yellow echo 'Running mypy static type checks...' && \
-mypy && \
+purple echo 'Running mypy static type checks...' && \
+mypy && echo && \
 
-lt_yellow echo 'Bandit is checking for security issues...' && \
-bandit -r -q . -x ./.venv,./.aws-sam/ --format screen && \
+purple echo 'Bandit is checking for security issues...' && \
+bandit -r -q . -x ./.venv,./.aws-sam/ --format screen && echo && \
 
-lt_yellow echo 'Running unit tests:' && \
-coverage run --branch --source=./ -m pytest tests && \
+purple echo 'Running unit tests:' && \
+coverage run --branch --source=./ -m pytest tests && echo && \
 
-lt_yellow echo 'Coverage report (fails under 100%)...' && \
-coverage report -m --fail-under=100 --omit=.venv && \
+purple echo 'Coverage report (fails under 100%)...' && \
+coverage report -m --fail-under=100 --omit=.venv && echo && \
 
 purple echo 'Pre-push checks complete.'
 
